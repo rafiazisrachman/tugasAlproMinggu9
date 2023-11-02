9bf26380bc8d34a08171d5be25d5d595062923c0
rafiazisrachman <149140188+rafiazisrachman@users.noreply.github.com>
2023-11-02 22:39:37 +0700

    Update tugasAlproMinggu9.py

--- tugasAlproMinggu9.py
+++ tugasAlproMinggu9.py
@@ -163,3 +163,79 @@ elif CrustPizza == "cheesy bites":
     elif sizePizza == "large":
         hargaPizza += 120909
         if cheese == "iya":
+ hargaPizza += 19091
+        elif cheese == "tidak":
+            hargaPizza += 0
+        else:
+            print("Baik, tidak menggunakan keju")
+    else:
+        print("Maaf tidak tersedia ukuran pizza lain")
+elif CrustPizza == "crown crust":
+    # Pelanggan memilih ukuran pizza
+    print("Ukuran pizza:")
+    print("personal")
+    print("regular")
+    print("large")
+    sizePizza = input("Silahkan pilih ukuran pizza anda: ")
+    #Apakah ingin menambah keju?
+    cheese = input("Ingin menambahkan keju? (iya/tidak): ")
+    if sizePizza == "personal":
+        hargaPizza += 11818
+        if cheese == "iya":
+            hargaPizza += 13636
+        elif cheese == "tidak":
+            hargaPizza += 0
+        else:
+            print("Baik, tidak menggunakan keju")
+    elif sizePizza == "regular":
+        hargaPizza += 77273
+        if cheese == "iya":
+            hargaPizza += 16364
+        elif cheese == "tidak":
+            hargaPizza += 0
+        else:
+            print("Baik, tidak menggunakan keju")
+    elif sizePizza == "large":
+        hargaPizza += 116363
+        if cheese == "iya":
+            hargaPizza += 19091
+        elif cheese == "tidak":
+            hargaPizza += 0
+        else:
+            print("Baik, tidak menggunakan keju")
+    else:
+        print("Maaf tidak tersedia ukuran pizza lain")
+else:
+    hargaPizza += 0
+    print("Maaf, Crust yang anda pilih tidak tersedia")
+
+
+# Total harga ditambah biaya pajak restoran
+taxs = 1.0
+totalharga = hargaPizza
+TotalTaxs = totalharga * (taxs / 100)
+TotalhargaPizza = totalharga + TotalTaxs
+
+#cara pembayaran
+print ("Silahkan pilih pembayaran : ")
+print("Tunai, Kartu Kredit, Kartu Debit")
+pembayaran = input()
+if pembayaran == "Tunai" or pembayaran == "Kartu Kredit" or pembayaran == "Kartu Debit":
+    if pembayaran == "Tunai":
+        TotalhargaPizza += 0
+elif pembayaran == "Kartu Kredit":
+    TotalhargaPizza += 2500
+
+#menampilkan nota pembelian customer
+print("========================")
+print("""YOURS RECEPTION""")
+print("Order Pizza with this detail :")
+print("Your topping : ", MenuPizza
+    , "Your Crust : ", CrustPizza
+    , "Your Size : ", sizePizza
+    , "Extra Cheese : ", cheese)
+print("Payment : ", pembayaran)
+print("Total : Rp.", totalharga)
+print("Taxs : {: .2f}%" .format(TotalTaxs))
+print("Your Bill : Rp.", TotalhargaPizza)
+print("=========================")
